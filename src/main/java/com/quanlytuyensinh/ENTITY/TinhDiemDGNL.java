f72289d7306807610ea5c8c93b8b2116ac34bfce
Tuan Khoi <9tuankhoi6@gmail.com>
2026-05-21 10:06:46 +0700

    Tính điểm tham khảo DGNL

--- src/main/java/com/quanlytuyensinh/ENTITY/TinhDiemDGNL.java
+++ src/main/java/com/quanlytuyensinh/ENTITY/TinhDiemDGNL.java
@@ -3,52 +3,35 @@ package com.quanlytuyensinh.ENTITY;
 import java.math.BigDecimal;
 
 public class TinhDiemDGNL {
-
     private String tenNganh;
-    private String maToHop;
-    private boolean toHopGoc;
+    private String toHopGoc;
     private BigDecimal diemThi;
     private String ctQuyDoi;
     private BigDecimal diemThiQuyDoi;
     private BigDecimal diemCong;
     private BigDecimal diemUuTien;
     private BigDecimal diemXetTuyen;
-    private BigDecimal doLech;
 
-public TinhDiemDGNL(String tenNganh,
-                    String maToHop,
-                    boolean toHopGoc,
-                    BigDecimal diemThi,
-                    String ctQuyDoi,
-                    BigDecimal diemThiQuyDoi,
-                    BigDecimal diemCong,
-                    BigDecimal diemUuTien,
-                    BigDecimal diemXetTuyen,
-                    BigDecimal doLech) {
-
-    this.tenNganh = tenNganh;
-    this.maToHop = maToHop;
-    this.toHopGoc = toHopGoc;
-    this.diemThi = diemThi;
-    this.ctQuyDoi = ctQuyDoi;
-    this.diemThiQuyDoi = diemThiQuyDoi;
-    this.diemCong = diemCong;
-    this.diemUuTien = diemUuTien;
-    this.diemXetTuyen = diemXetTuyen;
-    this.doLech = doLech;
-}
-
-    public TinhDiemDGNL() {
+    public TinhDiemDGNL(String tenNganh, String toHopGoc, BigDecimal diemThi, String ctQuyDoi, BigDecimal diemThiQuyDoi,
+            BigDecimal diemCong, BigDecimal diemUuTien, BigDecimal diemXetTuyen) {
+        this.tenNganh = tenNganh;
+        this.toHopGoc = toHopGoc;
+        this.diemThi = diemThi;
+        this.ctQuyDoi = ctQuyDoi;
+        this.diemThiQuyDoi = diemThiQuyDoi;
+        this.diemCong = diemCong;
+        this.diemUuTien = diemUuTien;
+        this.diemXetTuyen = diemXetTuyen;
     }
 
-    // ================= GETTER =================
+    public TinhDiemDGNL() {};
 
     public String getTenNganh() {
         return tenNganh;
     }
 
-    public String getMaToHop() {
-        return maToHop;
+    public String getToHopGoc() {
+        return toHopGoc;
     }
 
     public BigDecimal getDiemThi() {
@@ -73,71 +56,5 @@ public TinhDiemDGNL(String tenNganh,
 
     public BigDecimal getDiemXetTuyen() {
         return diemXetTuyen;
-    }
-
-    // ================= SETTER =================
-
-    public void setTenNganh(String tenNganh) {
-        this.tenNganh = tenNganh;
-    }
-
-    public void setMaToHop(String maToHop) {
-        this.maToHop = maToHop;
-    }
-
-    public void setDiemThi(BigDecimal diemThi) {
-        this.diemThi = diemThi;
-    }
-
-    public void setCtQuyDoi(String ctQuyDoi) {
-        this.ctQuyDoi = ctQuyDoi;
-    }
-
-    public void setDiemThiQuyDoi(BigDecimal diemThiQuyDoi) {
-        this.diemThiQuyDoi = diemThiQuyDoi;
-    }
-
-    public void setDiemCong(BigDecimal diemCong) {
-        this.diemCong = diemCong;
-    }
-
-    public void setDiemUuTien(BigDecimal diemUuTien) {
-        this.diemUuTien = diemUuTien;
-    }
-
-    public void setDiemXetTuyen(BigDecimal diemXetTuyen) {
-        this.diemXetTuyen = diemXetTuyen;
-    }
-    
-        // GETTER
-    public boolean getToHopGoc() {
-        return toHopGoc;
-    }
-
-    public BigDecimal getDoLech() {
-        return doLech;
-    }
-
-    // SETTER
-    public void setToHopGoc(boolean toHopGoc) {
-        this.toHopGoc = toHopGoc;
-    }
-
-    public void setDoLech(BigDecimal doLech) {
-        this.doLech = doLech;
-    }
-
-    @Override
-    public String toString() {
-        return "TinhDiemDGNL{" +
-                "tenNganh='" + tenNganh + '\'' +
-                ", maToHop='" + maToHop + '\'' +
-                ", diemThi=" + diemThi +
-                ", ctQuyDoi='" + ctQuyDoi + '\'' +
-                ", diemThiQuyDoi=" + diemThiQuyDoi +
-                ", diemCong=" + diemCong +
-                ", diemUuTien=" + diemUuTien +
-                ", diemXetTuyen=" + diemXetTuyen +
-                '}';
     }
 }
